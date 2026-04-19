977cdf55c3c0fab2850fd38b1085a9d72cf57905
kzs7jgyrfc-cmyk <4wolvrine23@gmail.com>
2026-04-18 19:38:31 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-// future search/edit features can go here
-console.log("Inventory Page Loaded");
+// Auto-save quantities in browser
+
+window.onload = () => {
+  const cells = document.querySelectorAll(".qty");
+
+  cells.forEach((cell, index) => {
+    const saved = localStorage.getItem("qty-" + index);
+    if (saved) cell.innerText = saved;
+
+    cell.addEventListener("input", () => {
+      localStorage.setItem("qty-" + index, cell.innerText);
+    });
+  });
+};
